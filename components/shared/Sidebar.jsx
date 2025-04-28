a06eb5ca082aa585c6e10ed2574c6caab0672314
Omenogor01 <samuelomenogor33@gmail.com>
2025-04-29 01:47:27 +0300

    Delete components directory

--- components/shared/Sidebar.jsx
+++
@@ -1,8 +0,0 @@
-export const Sidebar = ({ children }) => (
-    <div className="w-64 bg-white border-r border-gray-200 p-4">
-      <div className="space-y-4">
-        <h3 className="font-bold mb-2">Quick Links</h3>
-        {children}
-      </div>
-    </div>
-  );
